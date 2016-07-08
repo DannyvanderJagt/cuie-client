33ba4cca791c41da63419af06e04b505cefd3cd6
Danny van der Jagt <dannyvanderjagt@gmail.com>
2016-07-08 15:03:01 +0200

    [Birth] I'm alive and ready for improvements.

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+class Client{
+  constructor(cuie, key){
+    this.cuie = cuie;
+    this.key = key;
+  }
+
+
+}
+
+export default Client;
---
+++ src/service/index.js
@@ -0,0 +1,10 @@
+class Service{
+  constructor(cuie, key){
+    this.cuie = cuie;
+    this.key = key;
+  }
+
+
+}
+
+export default Service;
